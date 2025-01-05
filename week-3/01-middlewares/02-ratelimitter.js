df876c779dc29771c86ee81937dca766faef5b76
FaizanMalek <fmalek01@mynbcc.ca>
2025-01-05 03:54:44 -0400

    completed Middleware Asns

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,19 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+
+app.use((req,res,next)=>{
+  const userId = req.headers['user-id'];
+  if(numberOfRequestsForUser[userId]){
+    numberOfRequestsForUser[userId]++;
+    if(numberOfRequestsForUser[userId]>5) return res.status(404).json({msg: "blocked"});
+  }
+  else {
+    numberOfRequestsForUser[userId]=1;    
+  }
+  next();
+})
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -23,4 +23,13 @@ app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
 
+app.use((err,req,res,next)=>{
+  if(err){
+    errorCount++;
+    res.status(404).json({
+      msg: "Fatal error",
+    })
+  }
+})
+
 module.exports = app;
