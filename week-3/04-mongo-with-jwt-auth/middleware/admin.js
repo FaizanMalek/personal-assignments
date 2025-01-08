8488f9ddc028dfe263f4911e655cecc0a6bfa934
FaizanMalek <fmalek01@mynbcc.ca>
2025-01-08 01:06:11 -0400

    userMiddleware with jwt Auth

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -4,7 +4,7 @@ async function adminMiddleware (req, res, next) {
     // You need to check the headers and validate the admin from the admin DB.
     // Check readme for the exact headers to be expected
     const token = req.headers.authorization;
-    const words = token.split("");
+    const words = token.split(" ");
     const jwtToken = words[1];
     const decoded = jwt.verify(jwtToken, JWT_SECRET);
     try{
--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,20 @@
 function userMiddleware(req, res, next) {
     // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    // You need to check the headers and validate the user from the user DB.
+    // Check readme for the exact headers to be expected
+    const token = req.headers.authorization;
+    const words = token.split(" ");
+    const jwtToken = words[1];
+
+    const decoded = jwt.verify(jwtToken, JWT_SECRET);
+    try{
+        if(!decoded.username) return res.status(403).json({
+            msg: 'Could not be authenticated'
+        })
+        next();
+    } catch(err){
+         res.json({msg: 'incorrect inputs'})
+        }
 }
 
 module.exports = userMiddleware;
