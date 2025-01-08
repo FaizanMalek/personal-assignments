683d08f1250dba1b1384968807638f4b409cfb67
FaizanMalek <fmalek01@mynbcc.ca>
2025-01-08 00:56:29 -0400

    admin middleware jwtToken authorization

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,18 @@
 // Middleware for handling auth
-function adminMiddleware(req, res, next) {
+async function adminMiddleware (req, res, next) {
     // Implement admin auth logic
-    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    // You need to check the headers and validate the admin from the admin DB.
+    // Check readme for the exact headers to be expected
+    const token = req.headers.authorization;
+    const words = token.split("");
+    const jwtToken = words[1];
+    const decoded = jwt.verify(jwtToken, JWT_SECRET);
+    try{
+        if(!decoded.username) return res.status(400).json("jwt could not be verified");
+    next();
+    } catch(err){
+        res.json({msg: 'incorrect inputs'})
+    }
 }
 
 module.exports = adminMiddleware;
